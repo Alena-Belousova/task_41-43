c524fa6d76eda485f6a0c6d98b4749219b484de8
Alena-Belousova <107402810+Alena-Belousova@users.noreply.github.com>
2022-07-11 19:48:45 +0300

    замена задачи 41

---
+++ task_41 new.cs
@@ -0,0 +1,16 @@
+﻿// Пользователь вводит с клавиатуры M чисел.
+// Посчитайте, сколько чисел больше 0 ввёл пользователь
+Console.WriteLine("задача 41");
+Console.WriteLine("введите несколько чисел: ");
+string numbers = Console.ReadLine();
+string[] array = numbers.Split(',');
+string a= string.Join(",", array);
+Console.WriteLine("["+a+"]");
+int count =0;
+for(int i=0; i<array.Length; i++)
+{int b = Convert.ToInt32(array[i]);
+    if(b>0)
+    {count++;
+    }
+}
+Console.WriteLine("кол-во положительных: "+count);
